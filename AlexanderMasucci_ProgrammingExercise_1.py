fc620991c4a7c7238154ccacc3e5a43cabb79e19
Alexander Masucci <masuccialexander@gmail.com>
2025-09-02 12:51:00 -0400

    Version Control Exercise change #3: Accumulator renamed from "tickets" to "tic_total."

--- AlexanderMasucci_ProgrammingExercise_1.py
+++ AlexanderMasucci_ProgrammingExercise_1.py
@@ -6,21 +6,21 @@ def main():
     print('Cinema Ticket Pre-Sale Simulator.')
 
     # Number of tickets remaining
-    tickets = 10
+    tic_total = 10
 
     # Loop until no tickets remain
-    while tickets > 0:
+    while tic_total > 0:
         print()
-        print(f'There are {tickets} tickets remaining.')
+        print(f'There are {tic_total} tickets remaining.')
 
         # Call function and set new total ticket value
-        tickets = tic_input(tickets)
+        tic_total = tic_input(tic_total)
 
     print('All tickets have been purchased. Thank you for your time.')
 
 
 # First function prompts user to buy tickets and subtracts it from total.
-def tic_input(tickets):
+def tic_input(tic_total):
 
     # Preamble.
     print()
@@ -31,26 +31,26 @@ def tic_input(tickets):
         ticket_buy = int(input())
     except:
         print('Please input a number.')
-        return tickets
+        return tic_total
 
 
     # Check if there are enough tickets remaining.
-    if ticket_buy > tickets:
+    if ticket_buy > tic_total:
         print('Sorry, there are not enough tickets remaining.')
-        return tickets
+        return tic_total
 
     # Check if there was a valid number of tickets input.
     if ticket_buy in range(0, 5):
         # Subtract tickets bought from total tickets
-        tickets -= ticket_buy
+        tic_total -= ticket_buy
         print(f'You have purchased {ticket_buy} tickets.')
         # Return new value
-        return tickets
+        return tic_total
     elif ticket_buy > 4:
         print('Please purchase only 4 tickets or less.')
-        return tickets
+        return tic_total
     else:
         print('Invalid input. Please try again.')
-        return tickets
+        return tic_total
 
 main()
